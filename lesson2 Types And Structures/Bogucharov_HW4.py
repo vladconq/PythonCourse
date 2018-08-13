3804e5bf3a50bc0c45789a5a78d72c2b4271ff82
vladconq <vladconq@gmail.com>
2018-08-13 12:06:02 +0300

    hw4 without try/except

--- lesson2 Types And Structures/Bogucharov_HW4.py
+++ lesson2 Types And Structures/Bogucharov_HW4.py
@@ -3,11 +3,10 @@ while True:
     if text == 'cancel':
         break
     source_numbers = [(int(i)) for i in text.split()]
-    all_numbers_from_min_to_max = range(1, max(source_numbers))
-    try:
-        number = min(set(all_numbers_from_min_to_max) - set(source_numbers))
-    except ValueError:
+    all_numbers_from_min_to_max = list(range(1, max(source_numbers) + 1))
+    new_set = set(all_numbers_from_min_to_max) - set(source_numbers)
+    if not new_set:
         print(max(source_numbers) + 1)
     else:
-        print(number)
+        print(min(new_set))
 print("Bye!")
