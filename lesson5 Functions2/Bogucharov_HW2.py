565b299d94531bfa062279ff5b3062c2d3ef2b45
vladconq <vladconq@gmail.com>
2018-08-19 15:39:09 +0300

    Edit lesson2 and lesson5

--- lesson5 Functions2/Bogucharov_HW2.py
+++ lesson5 Functions2/Bogucharov_HW2.py
@@ -1,7 +1,16 @@
-def mysqrt(num, eps=0.001, estimation=1):
+def mysqrt(num, eps=0.0000001, estimation=1):
     new_estimation = (num / estimation + estimation) / 2
     if abs(new_estimation - estimation) <= eps:
-        print(round(new_estimation, 2))
+        if eps != 0.0000001:
+            print(round(new_estimation, len(str(eps)) - 2))
+        else:
+            index = None
+            str_new_est = str(new_estimation)
+            for i in range(len(str_new_est)):
+                if str_new_est[i].isdigit() and str_new_est[i] != '0':
+                    index = i
+                    break
+            print(round(new_estimation, index))
         return
     mysqrt(num, eps, new_estimation)
 
@@ -9,3 +18,5 @@ def mysqrt(num, eps=0.001, estimation=1):
 mysqrt(4)
 mysqrt(5, 0.01)
 mysqrt(4 / 10000)
+mysqrt(4 / 1000000)
+mysqrt(100)
--- lesson5 Functions2/Bogucharov_HW5.py
+++ lesson5 Functions2/Bogucharov_HW5.py
@@ -1,4 +1,5 @@
 import time
+from math import sqrt
 
 dict_of_results = {'total calls': 0, 'total time': 0}
 
@@ -26,8 +27,13 @@ def profiling_decorator(global_var_of_time_and_calls):
                 dict_of_each_func[func.__name__] = total_time
                 print(dict_of_each_func)
                 # print(value)
+                if globals()[global_var_of_time_and_calls]['total time'] == 0 and \
+                        globals()[global_var_of_time_and_calls][
+                            'total calls'] > 1:
+                    print("Both methods in this case have the same result.")
+                    return
                 print("Now optimal: " + str(min(dict_of_each_func.items(), key=lambda x: x[1])))
-                return value
+                return
 
         return wrapper
 
@@ -60,16 +66,81 @@ def fib_rec(num):
         return fib_rec(num - 1) + fib_rec(num - 2)
 
 
+def pow(x, n, I, mult):
+    if n == 0:
+        return I
+    elif n == 1:
+        return x
+    else:
+        y = pow(x, n // 2, I, mult)
+        y = mult(y, y)
+        if n % 2:
+            y = mult(x, y)
+        return y
+
+
+def identity_matrix(n):
+    r = list(range(n))
+    return [[1 if i == j else 0 for i in r] for j in r]
+
+
+def matrix_multiply(A, B):
+    BT = list(zip(*B))
+    return [[sum(a * b
+                 for a, b in zip(row_a, col_b))
+             for col_b in BT]
+            for row_a in A]
+
+
+@profiling_decorator('dict_of_results')
+def fib_matrix(num):
+    try:
+        F = pow([[1, 1], [1, 0]], num, identity_matrix(2), matrix_multiply)
+        return F[0][1]
+    except OverflowError:
+        return "Result too large"
+
+
+@profiling_decorator('dict_of_results')
+def fib_binet(num):
+    try:
+        return int((((1 + sqrt(5)) ** num) - ((1 - sqrt(5)) ** num)) / ((2 ** num) * sqrt(5)))
+    except OverflowError:
+        return "Result too large"
+
+
+# Compare fib_rec and fib_loop
 fib_rec(20)
 print()
 fib_loop(20)
 print()
 print(dict_of_results)
-print()
+# Ok, fib_loop is better
 dict_of_results = {'total calls': 0, 'total time': 0}
+print()
 
+# Now compare fib_loop and fib_binet
+fib_loop(500)
+print()
+fib_binet(500)
+print()
+print(dict_of_results)
+# Ok, in this case they are the same
+dict_of_results = {'total calls': 0, 'total time': 0}
+print()
+
+# Now compare fib_list and fib_loop
 fib_list(1000)
 print()
 fib_loop(1000)
+print()
+print(dict_of_results)
+print()
+dict_of_results = {'total calls': 0, 'total time': 0}
+
+# Now compare fib_loop and fib_matrix
+fib_loop(100000)
+print()
+fib_matrix(100000)
 print()
 print(dict_of_results)
