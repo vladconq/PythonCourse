aa65cb110890fa5f47d3ed2f7152a4c5cc46d9aa
vladconq <vladconq@gmail.com>
2018-09-22 17:44:17 +0300

    except

--- lesson Exceptions/hw1.py
+++ lesson Exceptions/hw1.py
@@ -1,26 +1,32 @@
+import sys
+from contextlib import redirect_stderr
+from contextlib import redirect_stdout
+
+
 def stderr_redirect(dest=None):
     def real_decorator(func):
         def wrapper():
-            if dest == None:
-                result = func()
-                print(result)
-            else:
-                with open(dest, 'w') as fd:
-                    try:
-                        result = func()
-                    except Exception as error:
-                        print(error, file=fd)
-                    else:
-                        print(result, file=fd)
+            with open(dest, 'a+') as f:
+                with redirect_stderr(f):
+                    return func()
 
         return wrapper
 
     return real_decorator
 
 
-@stderr_redirect(dest='output')
-def func_with_error():
-    return ("1234" + 1)
+@stderr_redirect(dest='file1')
+def test1():
+    sys.stderr.write('test1 func\n')
+    return 10
 
 
-func_with_error()
+@stderr_redirect(dest='file2')
+def test2():
+    test1()
+    sys.stderr.write('test2 func\n')
+
+    return 10 + test1()
+
+
+print(test2())
