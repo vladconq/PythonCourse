8bb50d81cab65982cbe4a9ff96874677e6770210
vladconq <vladconq@gmail.com>
2018-09-24 01:11:47 +0300

    second version

---
+++ lesson Functions2/HW2_CORRECT 2.py
@@ -0,0 +1,20 @@
+def my_sqrt(n, precision=None):
+    if not precision:
+        if n < 1:
+            precision = n
+        else:
+            precision = 0
+    lo, hi = 0.0, max(n, 1.0)
+    prev, mid = 0, (lo + hi) / 2.0
+    while abs(mid - prev) > precision:
+        prev, mid = mid, (mid + (n / mid)) / 2.0
+    return '{:.3g}'.format(mid)
+
+
+print(my_sqrt(4))
+print(my_sqrt(5, 0.01))
+print(my_sqrt(4 / 10000))
+print(my_sqrt(4 / 1000000))
+print(my_sqrt(100))
+print(my_sqrt(10e6))
+print(my_sqrt(4e-128))
